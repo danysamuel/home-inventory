227859e135d58a2e6267f8cfab374356efefc41f
Dany Samuel <danysamuel@gmail.com>
2026-05-06 22:51:36 +1000

    added logger

--- src/main/java/com/example/homeinventory/web/RoomController.java
+++ src/main/java/com/example/homeinventory/web/RoomController.java
@@ -1,11 +1,14 @@
 package com.example.homeinventory.web;
 
+import com.example.homeinventory.model.Room;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.homeinventory.repository.RoomRepository;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/rooms")
@@ -19,7 +22,10 @@ public class RoomController {
 
     @GetMapping
     public String listRooms(Model model) {
-        model.addAttribute("rooms", roomRepository.findAll());
+
+        List<Room> rooms = roomRepository.findAll();
+        System.out.println("rooms size: " + rooms.size());
+        model.addAttribute("rooms", rooms);
         return "rooms/list";
     }
 }
